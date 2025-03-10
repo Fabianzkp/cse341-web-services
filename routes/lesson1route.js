f648d35b716c4c7c015e1dd155791b2188ff53bf
Fabianzkp <fabianzkp@gmail.com>
2025-03-10 23:37:36 +0100

    check

--- routes/lesson1route.js
+++
@@ -1,8 +0,0 @@
-const routes = require('express').Router();
-const lesson1Controller = require('../controllers/lesson1');
-
-routes.get('/', lesson1Controller.ananaRoute);
-
-routes.get('/fabian', lesson1Controller.fabianRoute);
-
-module.exports = routes;  // Export the routes to be used in server.js 
